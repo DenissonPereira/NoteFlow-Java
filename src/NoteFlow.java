e6ab90fcec7925170ee24d199d521475d7f12eee
Denisson Pereira Santos <denisson159@gmail.com>
2023-10-28 18:13:14 -0300

    public class

--- src/NoteFlow.java
+++ src/NoteFlow.java
@@ -1,5 +1,27 @@
-// Importando bibliotecas
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 
 
+class Post {
+    private String title;
+    private String content;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+}
+
+public class NoteFlow {
+
+    private List<Post> posts = new ArrayList<>();
+}
